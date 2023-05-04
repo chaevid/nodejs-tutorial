476a579946af1a8dc2b1d9215f9e8da53a83bae7
chaevid <chaevid@gmail.com>
2023-05-04 17:38:42 +0900

    Back to the basic

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-//////
-
 const express = require('express');
-const multer = require('multer');
-const { convertFBXtoGLTF } = require('./converter');
-const rimraf = require('rimraf'); // Add this line to the top of the file to use the rimraf module
-
 const app = express();
-const upload = multer({ dest: 'uploads/' });
 
-app.use(express.static('public'));
-
-app.post('/convert', upload.single('file'), async (req, res) => {
-  try {
-    const { outputFolder, outputGltfPath } = await convertFBXtoGLTF(
-      req.file.path
-    );
-    res.download(outputGltfPath, (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error downloading file.');
-      } else {
-        rimraf.sync(outputFolder);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(`Error: ${error.message}`);
-  }
+app.get('/', (req, res) => {
+  res.send('Hello Node JS!');
 });
 
 const PORT = process.env.PORT || 3000;
